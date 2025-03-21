8bfe836c9120aba573a62addcb9f25281b08b136
krishna <krishnakumar.snm792004@gmail.com>
2025-03-21 19:48:55 +0530

    Initial commit

--- Array_problem/class_02.java
+++ Array_problem/class_02.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
-public class SumNaturalNumbers {
+public class SumWholeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a positive number: ");
         int n = scanner.nextInt();
 
-        if (n <= 0) {
-            System.out.println("Please enter a positive natural number.");
+        if (n < 0) {
+            System.out.println("Please enter a positive whole number.");
         } else {
             int sum = 0;
-
-            for (int i = 1; i <= n; i++) {
+            
+            for (int i = 0; i <= n; i++) {
                 sum += i;
             }
-
-            System.out.println("Sum of natural numbers from 1 to " + n + " is: " + sum);
+            
+            System.out.println("Sum of whole numbers from 0 to " + n + " is: " + sum);
         }
 
         scanner.close();
